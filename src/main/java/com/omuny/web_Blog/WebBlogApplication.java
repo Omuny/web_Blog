77c73ff8c1834443c0e3659bd8bd5747cb2f5a13
omuny <omuny@yandex.ru>
2025-05-26 20:25:42 +0300

    Инициализация проекта на Spring Boot. Добавление базового наполнения

---
+++ src/main/java/com/omuny/web_Blog/WebBlogApplication.java
@@ -0,0 +1,13 @@
+package com.omuny.web_Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBlogApplication.class, args);
+	}
+
+}
