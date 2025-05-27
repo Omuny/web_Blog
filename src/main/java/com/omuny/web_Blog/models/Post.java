df100fbb1c04fde8153cce37c236d5e6d7e0f4c2
omuny <omuny@yandex.ru>
2025-05-27 17:00:20 +0300

    Создан функционал для добавления статей в базу данных через форму на сайте. Создана форма, обработчики для получения данных, а также сохранение новых статей в MySQL базу данных

--- src/main/java/com/omuny/web_Blog/models/Post.java
+++ src/main/java/com/omuny/web_Blog/models/Post.java
@@ -56,4 +56,12 @@ public class Post {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Post() {}
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
 }
